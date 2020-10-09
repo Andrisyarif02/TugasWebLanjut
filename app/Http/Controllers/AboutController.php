7a96bbdd798caca2dc9bfcf1dd409d2435e91aa9
Andrisyarif02 <andrisyarif02@gmail.com>
2020-10-09 10:54:25 +0700

    commit

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
-{
+{  
     public function about(){
         return view('about');
         $value = Cache::rememberForever('articles', function(){
