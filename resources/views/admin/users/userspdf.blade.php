11cc2c36160916e45ed0cc2cbdb55b0c5a66d48a
Andrisyarif02 <andrisyarif02@gmail.com>
2020-11-26 09:10:44 +0700

    KUISPWL2

--- resources/views/admin/users/userspdf.blade.php
+++ resources/views/admin/users/userspdf.blade.php
@@ -1,18 +1,41 @@
 <html>
 <head>
     <title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
+    
 </head>
 <body>
     <style type="text/css">
-        table tr td,
-        table tr th{
-        font-size: 9pt;
+       .table1 {
+            font-family: sans-serif;
+            color: #444;
+            border-collapse: collapse;
+            width: 50%;
+            border: 1px solid #f2f5f7;
         }
+
+        .table1 tr th{
+            background: #35A9DB;
+            color: #fff;
+            font-weight: normal;
+        }
+
+        .table1, th, td {
+            padding: 8px 20px;
+        }
+
+        .table1 tr:hover {
+            background-color: #f5f5f5;r;
+        }
+
+        .table1 tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+    }
     </style>
     <center>
-        <h5>Laporan User</h4>
+        <h5>Laporan Data User</h4>
     </center>
-    <table class='table table-bordered'>
+    <table class='table1'>
         <thead>
             <tr>
                 <th>Name</th>
@@ -26,7 +49,7 @@
             @foreach($users as $b)
             <tr>
                 <td>{{$b->name}}</td>
-                <td><img width="200px" src="{{ public_path('storage/'.$b->image)}}" ></td>
+                <td><img width="100px" src="{{ public_path('storage/'.$b->image)}}" ></td>
                 <td>{{$b->email}}</td>
                 <td>{{$b->roles}}</td>
             </tr>
--- resources/views/articles_PDF.blade.php
+++ resources/views/articles_PDF.blade.php
@@ -4,10 +4,10 @@
 </head>
 <body>
     <style type="text/css">
-        table tr td,
-        table tr th{
+        table tr td{padding:2px 4px; background-color:rgba(84, 84, 92, 0.253); color:rgb(0, 0, 0);},
+        table tr th{padding:2px 4px; background-color:#007bff; color:rgb(0, 0, 0);}
         font-size: 9pt;
-        }
+    }
     </style>
     <center>
         <h5>Laporan Artikel</h4>
@@ -15,7 +15,6 @@
     <table class='table table-bordered'>
         <thead>
             <tr>
-                <th>No</th>
                 <th>Judul</th>
                 <th>Isi</th>
                 <th>Genre</th>
@@ -26,7 +25,6 @@
             @php $i=1 @endphp
             @foreach($articles as $a)
             <tr>
-                <td>{{ $i++ }}</td>
                 <td>{{$a->title}}</td>
                 <td>{{$a->content}}</td>
                 <td>{{$a->genre}}</td>
